3a4231ccf0ff10ed44673bcc7c913813e6c41666
oliur <oliurr941@gmail.com>
2022-03-19 18:22:36 +0600

    video-5

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Layout({ title, description, children }) {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                quia voluptates ipsam quae libero consequatur aut atque fuga
+                aliquam optio pariatur sed laboriosam dolores sunt, veritatis
+                incidunt corrupti, laborum similique!
+            </p>
+        </div>
+    );
+}
